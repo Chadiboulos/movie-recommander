27aad59605aa5b0b109e8e9c7be70562abdcdf7a
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2024-04-29 13:35:48 +0200

    add dag activation

--- database/application/create_database.py
+++ database/application/create_database.py
@@ -6,6 +6,9 @@ import os
 import re
 import datetime
 import time
+import requests
+import json
+import time
 dbname = os.environ.get("POSTGRES_DB")
 user = os.environ.get("POSTGRES_USER")
 password = os.environ.get("POSTGRES_PASSWORD")
@@ -212,4 +215,40 @@ print("refresh_table_recap_view")
 refresh_table_recap_view()
 print("Terminer !!!")
 print(datetime.datetime.now())
+
+
+airflow_api_url = os.environ.get("AIRFLOW_URL")
+airflow_user = os.environ.get("AIRFLOW_USER")
+airflow_usr_pwd = os.environ.get("AIRFLOW_USR_PWD")
+auth = (airflow_user, airflow_usr_pwd)
+airflow_is_run = False
+
+
+while not airflow_is_run:
+    response = requests.get(f"{airflow_api_url}/api/v1/dags",auth=auth)
+    if response.status_code == 200:
+        print ("Airflow is running!!!")
+        airflow_is_run = True
+    else:
+        print ("Airflow is not running yet !!!")
+        time.sleep(5)
+
+dagname= list()
+for dag in json.loads(response.text).get("dags"):
+    dagname.append(dag['dag_id'])
+print(dagname)
+
+
+for dag in dagname:
+    # Endpoint to activate a DAG
+    endpoint_activate = f'{airflow_api_url}/dags/{dag}'
+
+    activate_payload = {'is_paused': False}
+
+    # Send a PATCH request to activate the DAG
+    response = requests.patch(endpoint_activate, json=activate_payload, auth=auth)
+    if response.status_code == 200:
+        print("Dag {dag} activé !!!")
+    else:
+        print("Probleme a l'activation du dag {dag} !!!")
 
